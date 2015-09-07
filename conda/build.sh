33f4fc12c24bfddbfdaf9515cd3c65362a011bf3
Patrick Snape <patricksnape@gmail.com>
2015-09-07 15:32:14 +0100

    Steal options from conda-recipes/freeimage PR
    Fixes build errors on Travis

--- conda/build.sh
+++ conda/build.sh
@@ -1,6 +1,8 @@
+LANG="C" sed -i.bak 's:-o root -g root::' Makefile*
+
 make -f Makefile.gnu
-make -f Makefile.gnu install PREFIX=${PREFIX}
+make -f Makefile.gnu install PREFIX=${PREFIX} DESTDIR="${PREFIX}" INCDIR="${PREFIX}/include" INSTALLDIR="${PREFIX}/lib"
 
 make -f Makefile.fip
-make -f Makefile.fip install PREFIX=${PREFIX}
+make -f Makefile.fip install PREFIX=${PREFIX} DESTDIR="${PREFIX}" INCDIR="${PREFIX}/include" INSTALLDIR="${PREFIX}/lib"
 
