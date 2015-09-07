a32342ff012b83c734cbec58b6677cd95e4f9879
Patrick Snape <patricksnape@gmail.com>
2015-09-07 15:48:02 +0100

    Fix building on Linux/OSX with different seperators

--- conda/run_test.py
+++ conda/run_test.py
@@ -12,10 +12,12 @@ if sys.platform.startswith('win'):
     ext = '.dll'
     loader = ctypes.windll.LoadLibrary
 elif sys.platform.startswith('darwin'):
+    sep = '.'
     libdir = 'lib'
     fname_base = 'libfreeimage'
     ext = '.dylib'
 else:
+    sep = '-'
     libdir = 'lib'
     fname_base = 'libfreeimage'
     ext = '.so'
@@ -23,7 +25,7 @@ else:
 # Get paths to the lib
 libnames = []
 if not sys.platform.startswith('win'):
-    libnames.append(os.path.join(sys.prefix, libdir, fname_base + '.' + lib_version + ext))
+    libnames.append(os.path.join(sys.prefix, libdir, fname_base + sep + lib_version + ext))
 libnames.append(os.path.join(sys.prefix, libdir, fname_base + ext))
 
 # Load library and get found version
